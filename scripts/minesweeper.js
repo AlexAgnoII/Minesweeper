f47c3e73290ad78e401786703376443aefe2d50a
AlexAgnoII <3alex23@gmail.com>
2018-01-30 15:23:31 +0800

    title almost done

--- scripts/minesweeper.js
+++ scripts/minesweeper.js
@@ -42,16 +42,33 @@ let bombSprite,
     timerContainerSprite,
     unpressedBlockSprite;
 
+let buttonStart,
+    buttonStartContainer,
+    titleLogo,
+    titleBG;
+
+let playBG;
+
 let titleScene,
     playScene,
     gameOverScene;
 
 let state;
+let charm = new Charm(PIXI);
 
 gameDiv.appendChild(app.view);
 PIXI.loader
 .add(mineSweeperAtlas)
+.on("progress", loadProgressHandler)
 .load(setup);
+
+function loadProgressHandler(loader, resource) {
+  //Display the file `url` currently being loaded
+  console.log("loading: " + resource.url); 
+
+  //Display the percentage of files currently loaded
+  console.log("progress: " + loader.progress + "%"); 
+}
 
 function setup() {
     id = PIXI.loader.resources[mineSweeperAtlas].textures; 
@@ -67,28 +84,35 @@ function setup() {
 
 function gameLoop() {
     state();
+    charm.update();
 }
 
 
 function initializeTitle(){
-    titleScene = new PIXI.Container();
-    app.stage.addChild(titleScene);
-    
-    
-}
-function title() {}
-
-function initializePlay(){
-    let buttonStart;
+    //This button action serves as the "transition" for title -> play
     let buttonAction = function() {
-        console.log("Play!");  
+        console.log("Play!");
+        //reset and send to 'play'
+        initializeButton(buttonStart);
+        charm.fadeOut(titleScene).onComplete = () => {
+            buttonStartContainer.alpha = 0;
+            titleLogo.alpha = 0;
+            state = play
+        };
     };
     
-    playScene = new PIXI.Container();
-    app.stage.addChild(playScene);
+    titleScene = new PIXI.Container();
+    titleScene.alpha = 0;
+    app.stage.addChild(titleScene);
+    
+    titleBG = new PIXI.Sprite(id[spriteSource[8]]);
+    titleBG.anchor.set(0.5, 0.5);
+    titleBG.position.set(gameWidth / 2, gameHeight / 2);
+    titleScene.addChild(titleBG);
     
     buttonStart = new PIXI.Sprite(id[spriteSource[4]]);
     initializeButton(buttonStart);
+    buttonStart.anchor.set(0.5,0.5);
     buttonStart
     .on("pointerdown", () => buttonDown(buttonStart, id[spriteSource[2]]))
     .on("pointerover", () => buttonHover(buttonStart, id[spriteSource[3]]))
@@ -96,13 +120,41 @@ function initializePlay(){
                                     id[spriteSource[4]], 
                                     id[spriteSource[3]],
                                     buttonAction))
-    .on("pointerout", () => buttonOut(buttonStart, 
-                                      id[spriteSource[4]]));
-    playScene.addChild(buttonStart);
+    .on("pointerout", () => buttonOut(buttonStart, id[spriteSource[4]]))
+
+    
+    titleLogo = new PIXI.Sprite(id[spriteSource[7]]);
+    titleLogo.anchor.set(0.5,0.5);
+    titleLogo.position.set(gameWidth/2, gameHeight/2 - 50);
+    titleLogo.alpha = 0;
+    titleScene.addChild(titleLogo);
+    
+
+    buttonStartContainer = new PIXI.Container();
+    buttonStartContainer.position.set(gameWidth/2, gameHeight/2 + 50);
+    buttonStartContainer.alpha = 0;
+    buttonStartContainer.addChild(buttonStart);
+    titleScene.addChild(buttonStartContainer);
+    
+    charm.fadeIn(titleScene).onComplete = () => 
+    charm.fadeIn(titleLogo).onComplete = () => 
+    charm.fadeIn(buttonStartContainer).onComplete = () => activateButton(buttonStart);
+}
+function title() {
+
+}
+
+function initializePlay(){
+    playScene = new PIXI.Container();
+    app.stage.addChild(playScene);
     
     
 }
-function play() {}
+
+function play() {
+
+    
+}
 
 function initializeEnd(){
     gameOverScene = new PIXI.Container();
@@ -113,10 +165,15 @@ function end(){}
 
 
 function initializeButton(button) {
-    button.interactive = true;
-    button.buttonMode = true;
+    button.interactive = false;
+    button.buttonMode = false;
     button.isDown = false;
     button.isOver = false;
+}
+
+function activateButton(button) {
+    buttonStart.interactive = true;
+    buttonStart.buttonMode = true;
 }
 
 function buttonUp(sprite, textureUP, textureOver, action) {
@@ -130,7 +187,6 @@ function buttonUp(sprite, textureUP, textureOver, action) {
         sprite.texture = textureUP;
     }
     action();
-
 }
 function buttonHover(sprite, texture) {
     sprite.isOver = true;
